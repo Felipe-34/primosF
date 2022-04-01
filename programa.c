b41bfbe829cceb6b0a05fe8ad8134b2f3a34b3a0
Felipe Domingos <felipe.34domingos@gmail.com>
2022-03-31 21:47:57 -0300

    programa funcionando comitando para implementar alterações

--- programa.c
+++ programa.c
@@ -1,31 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #define tamanhoMax 18446744073709551615
-// Tenho que criar uma lógica para pular os numeros terminados em 5 porque todo o número terminado em 5 é divisivel por 5.
 
 typedef unsigned long long int ull;
 FILE *listaDePrimos;
 FILE *listaDeCompostos;
 FILE *ondeParou;
+FILE *quantidade;
 
 int Eprimo(ull numeroTeste);
 void checkfopen(FILE *Fptr);
 
 int main(){
 
-    int numeroDeTestes, testePrimo;
-    ull numeroTeste[1];
+    int testePrimo;
+    ull numeroDeTestes, numP[1], numC[1];
+    ull numeroTeste[1], valores;
+    clock_t tempo;
 
     ondeParou = fopen("ondeParou.bin", "rb");
     checkfopen(ondeParou);
     fread(numeroTeste, sizeof(ull), 1, ondeParou); 
     fclose(ondeParou);
 
+    quantidade = fopen("QuantidadePrimos.bin", "rb");
+    checkfopen(quantidade);
+    fread(numP, sizeof(ull), 1, quantidade);
+    fclose(quantidade);
+    quantidade = fopen("QuantidadeCompostos.bin", "rb");
+    checkfopen(quantidade);
+    fread(numC, sizeof(ull), 1, quantidade);
+    fclose(quantidade);
+    
     while(1){
         
         puts("Quantos numeros quer testar? Digite -1 para encerar o programa.");
-        scanf("%d", &numeroDeTestes);
+        scanf("%llu", &numeroDeTestes);
         
+        tempo = clock();
+
         if(numeroDeTestes == -1){
 
             return 0;
@@ -46,12 +60,13 @@ int main(){
                 checkfopen(listaDePrimos);
                 fwrite(numeroTeste, sizeof(ull), 1, listaDePrimos);
                 fclose(listaDePrimos);
+                numP[0]++;
             }else{
-
                 listaDeCompostos = fopen("lista de compostos.bin", "ab");
                 checkfopen(listaDeCompostos);
                 fwrite(numeroTeste, sizeof(ull), 1, listaDeCompostos);
                 fclose(listaDeCompostos);
+                numC[0]++;
             }
             
             if (i == numeroDeTestes - 1)
@@ -65,6 +80,17 @@ int main(){
             
         }
         
+        quantidade = fopen("QuantidadePrimos.bin", "wb");
+        checkfopen(quantidade);
+        fwrite(numP, sizeof(ull), 1, quantidade);
+        fclose(quantidade);
+        quantidade = fopen("QuantidadeCompostos.bin", "wb");
+        checkfopen(quantidade);
+        fwrite(numC, sizeof(ull), 1, quantidade);
+        fclose(quantidade);
+
+        tempo = clock(); - tempo;
+        printf("tempo de execução: %lf\n", ((float)tempo) / CLOCKS_PER_SEC);
     }
 
     return 0;
